31c506560f408b7623a539872f8619dab91ec7df
Cloud-BIT <106436336+Cloud-BIT@users.noreply.github.com>
2022-12-26 15:30:43 +0000

    code documentation

--- src/StocksNow/components/Sidebar.tsx
+++ src/StocksNow/components/Sidebar.tsx
@@ -7,4 +7,32 @@ const Sidebar: React.FC<Props> = () => {
   return (
     <div className='sidebar'>
       <div className='sidebar-heading-wrapp'>
-      
+        <p className='sidebar-heading'>
+          <Link to='/'>MarketCap</Link>
+        </p>
+      </div>
+      <div className='sidebar-list-wrapp'>
+        <ul>
+          <li>
+            <Link to='/latest-news' className='sidebar-link'>
+              <div>
+                <p>Latest News</p>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link to='/quotes' className='sidebar-link'>
+              <div>
+                <p>Quotes</p>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link to='/holders' className='sidebar-link'>
+              <div>
+                <p>Holders</p>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link to='/analysis' className='sidebar
