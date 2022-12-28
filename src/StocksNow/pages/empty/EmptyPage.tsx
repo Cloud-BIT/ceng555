b1b47646ec41afb011e2e34032e732507e8846ac
Cloud-BIT <106436336+Cloud-BIT@users.noreply.github.com>
2022-12-28 11:37:38 +0000

    pugs fudge

--- src/StocksNow/pages/empty/EmptyPage.tsx
+++ src/StocksNow/pages/empty/EmptyPage.tsx
@@ -1,1 +1,9 @@
-const EmptyPage = () =>
+const EmptyPage = () => (
+  <div className='flex-container'>
+    <div className='row'>
+      <div className='flex-item'>Empty page</div>
+    </div>
+  </div>
+);
+
+export default EmptyPage;
