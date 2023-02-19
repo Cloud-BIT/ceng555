fbc8e07dd5bced2e25a93284e9a95f8d30531139
Cloud-BIT <106436336+Cloud-BIT@users.noreply.github.com>
2023-02-19 17:37:06 +0000

    Fixes for linux

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import StocksNow from './StocksNow/StocksNow';
+import './StocksNow/styles/index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <StocksNow />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
